40e1cd0b54eb2fb4bde8948c4d263e37c7de8887
Harshit Jain <harshitjain382005@gmail.com>
2024-11-25 18:44:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ const cartItemsContainer = document.getElementById('cart-items');
 const cartTotal = document.getElementById('cart-total');
 let total = 0;
 
+document.body.style.transform = "scale(0.5)";
+document.body.style.transformOrigin = "top left";
+document.body.style.width = "200%";
+document.body.style.height = "200%";
+
 // Retrieve the cart from localStorage or initialize it
 const cart = JSON.parse(localStorage.getItem('cart')) || [];
 
